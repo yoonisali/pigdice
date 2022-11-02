2f9926080a2a674b0ca9168fe9dc471926b32c30
yoonisali <yoonismahamoudali@gmail.com>
2022-11-02 11:52:35 -0700

    add functions in js
    co-authored-by: brandonf55 <brandon.fenk99@gmail.com>
    
    co-authored-by: roonstott <roonstott@gmail.com>

--- README.md
+++ README.md
@@ -7,7 +7,24 @@ Code: function dice() {
     }
     dice();
 Expected Output: Random Number generated from 1-6
+
+Describe: function rollLogic()
+
+Test: "It should keep track of player score. If player rolls a "1" it returns 0. 
+Code: 
+
+
 ````
 
 
+
+On load, it is player 1's turn
+
+Roll dice button will generate random numnber and add that to the diceRoll. 
+Each time player presses roll dice button, value will either be added to diceRoll, or (if === 1), player will be kicked out of turn with score 0
+
+Hold button will add diceRoll to totalScore and the player will end their turn.
+
+
+Function controlTurn: The player will be kicked out of turn if the genrated number is 1. If playerOne turn === true, the diceRoll will show up in playerOne column, and the hold button will add diceRoll to playerOne totalScore. "Hold" button, or rolling a 1, will change playerOne turn value to false, and assign playerTwo turn value to true. 
 
--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,66 @@ function dice() {
     return Math.floor(Math.random() * 6) + 1
 }
 
+function Player(turn) {
+    this.totalScore = 0;
+    this.diceScore = 0;
+    this.turn = turn
+}
 
 
-// let dice = Math.floor(Math.random() * 6) + 1;
-// if (dice !== 1) {
-//     return dice;
-// } else if (dice === 1) {
-//     return null;
-// }
+let playerOne = new Player(true);
+let playerTwo = new Player(false);
 
+function whosTurn() {
+    let player;
+    if (playerOne.turn === true) {
+        player = "playerOne";
+    } else if (playerTwo.turn === true) {
+        player = "playerTwo";
+    }
+    return player;
+}
 
+function changeTurn() {
+    if (playerOne.turn === true) {
+        playerOne.turn = false;
+        playerTwo.turn = true;
+    } else {
+        playerOne.turn = true;
+        playerTwo.turn = false;
+    }
+}
 
+function rollLogic() {
+    let diceRoll = dice();
+    if (diceRoll !== 1) {
+        return diceRoll;
+    } else if (diceRoll === 1) {
+        changeTurn();
+        return "You rolled a 1!" + " " + whosTurn() + "'s" + " " + "turn!";
+    }
+}
 
+function holdButton() {
+    diceRoll += totalScore;
+    return playerTwo;
+}
 
 
 // UI Logic 
+
+window.addEventListener("load", function () {
+
+    document.querySelector(".btn-roll").addEventListener("click", rollLogic());
+    document.querySelector(".btn-reset").addEventListener("click", function () {
+        window.location.reload();
+    });
+    document.querySelector(".btn-hold").addEventListener("click", function () {
+
+    })
+
+})
+
+
+
+
