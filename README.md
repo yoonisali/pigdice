165cacbcfb81e606b00c3b24965c9d26d0d7c215
yoonisali <yoonismahamoudali@gmail.com>
2022-11-02 09:17:32 -0700

    initial commit with passing test for function dice()
    co-authored-by: brandonf55 <brandon.fenk99@gmail.com>
    co-authored-by: roonstott <roonstott@gmail.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+```
+Describe: function dice();
+
+Test: "It should generate a random number"
+Code: function dice() {
+    return Math.floor(Math.random() * 6) + 1;
+    }
+    dice();
+Expected Output: Random Number generated from 1-6
+````
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Buisness Logic
+
+function dice() {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+
+
+// let dice = Math.floor(Math.random() * 6) + 1;
+// if (dice !== 1) {
+//     return dice;
+// } else if (dice === 1) {
+//     return null;
+// }
+
+
+
+
+
+
+// UI Logic 
